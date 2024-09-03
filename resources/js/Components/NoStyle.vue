f436a1d4472652fbc64057f058668f649cd9d77c
Ruan Rita <54330181+Ruan-Rita@users.noreply.github.com>
2024-09-02 22:01:09 -0300

    CreateCampaign: add step 2 as draft

---
+++ resources/js/Components/NoStyle.vue
@@ -0,0 +1,12 @@
+<script setup>
+defineProps({
+    data: {}
+})
+</script>
+
+<template>
+    <div v-html="data"></div>
+</template>
+  
+  
+<style scoped></style>
--- resources/js/Pages/Dashboard/CreateCampaigns.vue
+++ resources/js/Pages/Dashboard/CreateCampaigns.vue
@@ -11,6 +11,7 @@ import axios from 'axios';
 import { QuillEditor } from '@vueup/vue-quill'
 import '@vueup/vue-quill/dist/vue-quill.snow.css';
 import '@vueup/vue-quill/dist/vue-quill.bubble.css';
+import NoStyle from '@/Components/NoStyle.vue';
 
 const formCampaign = useForm({
    title: 'ruan',
@@ -19,7 +20,7 @@ const formCampaign = useForm({
    hour: '14:20',
 });
 const stage = reactive({
-    step: 1,
+    step: 2,
     campaign: {}
 })
 
@@ -45,10 +46,21 @@ const getProgress = function () {
     }
     return '100%'
 }
+function log() {
+    console.log('SASDASDASD', typeof stage.campaign.content.ops[0].insert)
+}
+function renderContent() {
+    const data = stage.campaign.content.ops[0].insert
+    const finalValue = String(data).replace(/(?<!>)\s+(?!<)|&nbsp;/g, ' ');
+    console.log('TYPE OF DATA', data);
+    console.log('TYPE OF FINAL_VALUE', finalValue);
+    
+    return finalValue;
+}
 </script>
 <template>
     <Template title="Create Campaign">
-        <section class="max-w-7xl mx-auto sm:px-6 lg:px-8 py-8 flex flex-col gap-4 items-center">
+        <section v-if="stage.step == 0" class="max-w-7xl mx-auto sm:px-6 lg:px-8 py-8 flex flex-col gap-4 items-center">
             <div class="w-full flex">
                 <div class="w-[33%] font-extrabold text-app-blue text-center">Creating</div>
                 <div class="w-[33%] font-extrabold text-gray-400 text-center">Content</div>
@@ -57,7 +69,7 @@ const getProgress = function () {
             <div class="w-full bg-gray-300 h-5 rounded-lg">
                 <div :style="{width: getProgress()}" class="bg-app-green h-5 rounded-lg"></div>
             </div>
-            <section v-if="stage.step == 0" class="max-w-3xl flex flex-col gap-4 w-full">
+            <section  class="max-w-3xl flex flex-col gap-4 w-full">
                 <h1 class="font-bold my-10 text-2xl text-app-blue">Nova Campaigns</h1>
                 <div class="col-span-6 sm:col-span-4">
                     <InputLabel for="titleInput" value="Titulo" />
@@ -116,16 +128,58 @@ const getProgress = function () {
                     </PrimaryButton>
                 </div>
             </section>
-            <section v-if="stage.step == 1" class="max-w-3xl flex flex-col gap-4 w-full">
-                <h1 class="font-bold my-10 text-2xl text-app-blue">Criar conteudo</h1>
-                <div class="flex grap-2 bg-slate-500 min-h-96">
-                    <div>
-                        <QuillEditor v-model="stage.campaign.content" theme="snow"></QuillEditor>
+        </section>
+        <section v-if="stage.step == 1" class="max-w-7xl mx-auto sm:px-6 lg:px-8 py-8 flex flex-col gap-4 items-center">
+            <section class="max-w-full flex flex-col gap-4 w-full">
+                <div class="w-full flex">
+                    <div class="w-[33%] font-extrabold text-app-blue text-center">Creating</div>
+                    <div class="w-[33%] font-extrabold text-gray-400 text-center">Content</div>
+                    <div class="w-[33%] font-extrabold text-gray-400 text-center">Recipient</div>
+                </div>
+                <div class="w-full bg-gray-300 h-5 rounded-lg">
+                    <div :style="{width: getProgress()}" class="bg-app-green h-5 rounded-lg"></div>
+                </div>
+                <h1 class="font-bold mt-4 mb-4 text-2xl text-app-blue">Criar conteudo</h1>
+                <div class="flex gap-2 max-h-[1000px]">
+                    <div class="max-w-[600px] max-h-[556px]">
+                        <QuillEditor v-model:content="stage.campaign.content" :content="String" theme="snow"></QuillEditor>
                     </div>
-                    <div v-html="stage.campaign.content"></div>
+                    <div class="border w-[600px] min-h-[600px] flex-1 border-orange-400 p-5 rounded overflow-auto">
+                        <NoStyle v-if="stage.campaign.content && stage.campaign.content.ops" :data="renderContent()"></NoStyle>
+                        <!-- <iframe class="w-full min-h-[600px]" v-if="stage.campaign.content && stage.campaign.content.ops" :srcdoc="renderContent()" >
+                        </iframe> -->
+                    </div>
                 </div>
                 <div class="flex justify-end">
-                    <PrimaryButton @click="createCampaign" class="bg-app-blue" :class="{ 'opacity-25': formCampaign.processing }" :disabled="formCampaign.processing">
+                    <PrimaryButton @click="log" class="bg-app-blue" :class="{ 'opacity-25': formCampaign.processing }" :disabled="formCampaign.processing">
+                        Continue
+                    </PrimaryButton>
+                </div>
+            </section>
+        </section>
+        <section v-if="stage.step == 2" class="max-w-7xl mx-auto sm:px-6 lg:px-8 py-8 flex flex-col gap-4 items-center">
+            <section class="max-w-full flex flex-col gap-4 w-full">
+                <div class="w-full flex">
+                    <div class="w-[33%] font-extrabold text-app-blue text-center">Creating</div>
+                    <div class="w-[33%] font-extrabold text-gray-400 text-center">Content</div>
+                    <div class="w-[33%] font-extrabold text-gray-400 text-center">Recipient</div>
+                </div>
+                <div class="w-full bg-gray-300 h-5 rounded-lg">
+                    <div :style="{width: getProgress()}" class="bg-app-green h-5 rounded-lg"></div>
+                </div>
+                <h1 class="font-bold mt-4 mb-4 text-2xl text-app-blue">Criar conteudo</h1>
+                <div class="flex gap-2 max-h-[1000px]">
+                    <div class="max-w-[600px] max-h-[556px]">
+                        <QuillEditor v-model:content="stage.campaign.content" :content="String" theme="snow"></QuillEditor>
+                    </div>
+                    <div class="border w-[600px] min-h-[600px] flex-1 border-orange-400 p-5 rounded overflow-auto">
+                        <NoStyle v-if="stage.campaign.content && stage.campaign.content.ops" :data="renderContent()"></NoStyle>
+                        <!-- <iframe class="w-full min-h-[600px]" v-if="stage.campaign.content && stage.campaign.content.ops" :srcdoc="renderContent()" >
+                        </iframe> -->
+                    </div>
+                </div>
+                <div class="flex justify-end">
+                    <PrimaryButton @click="log" class="bg-app-blue" :class="{ 'opacity-25': formCampaign.processing }" :disabled="formCampaign.processing">
                         Continue
                     </PrimaryButton>
                 </div>
